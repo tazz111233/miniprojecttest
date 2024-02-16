d86ddc41a711bb9f3aa118b3c3e61803dedc3f88
hhabiiba <hbmdhb.2003@gmail.com>
2024-02-15 17:57:46 -0800

    Setup and Login.jsx

---
+++ front-end/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../index.css';
+import Login from './Login';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Currency Converter</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App; 
+
